2d3fff9b0a9f30778454cf08fd425ee4f812f5f4
Lucas Gabriel <lucas.gabriel@deway.com.br>
2019-11-24 23:09:07 -0300

    add app users

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    name = models.CharField(verbose_name='Nome', max_length=100)
+    email = models.EmailField(verbose_name='E-mail', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
